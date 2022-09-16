065d67ea49eec37cbbb562374cb66c82aa4d190b
Juanbriones <Juanbriones1454@hotmail.com>
2022-09-16 10:07:23 -0500

    completado

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using trabajo1.Models;
+
+namespace trabajo1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult MiPerfil(PerfilViewModel perfil)
+        {
+            return View(perfil);
+        }
+    }
+}
---
+++ Models/PerfilViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace trabajo1.Models
+{
+    public class PerfilViewModel
+    {
+        public string? Nombre { get; set; }
+        public int Semestre { get; set; }
+        public string? Materia { get; set; }
+        public string? Periodo { get; set; }
+
+
+    }
+}
